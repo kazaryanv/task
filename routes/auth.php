cb01ac962367922d121d57d10bf137a50b40f514
kazaryanv <kazaryanv01@mail.ru>
2022-03-27 23:46:54 +0300

    admin from login page

--- routes/auth.php
+++ routes/auth.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\LoginController;
 
 Route::get("/login", [LoginController::class, 'index'])->name("login");
 Route::post("/login/store", [LoginController::class, 'login'])->name("login-view");
-Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 Route::get("/admin", function(){
     return view('admin.dashboard');
 })->middleware("auth")->name('dashboard');
