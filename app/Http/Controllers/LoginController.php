f5ddef82ba21bba8b977272f6ac8c0baca46fdf7
kazaryanv <kazaryanv01@mail.ru>
2022-03-28 17:55:20 +0300

    logout funcktion

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -36,6 +36,6 @@ class LoginController extends Controller
 
     public function  logout() {
         Auth::logout();
-        return redirect()->route('login-view');
+        return redirect()->route('login');
     }
 }
